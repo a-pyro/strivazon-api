783760856f2e9832e01109a8418b1a734499df94
Mihai Ivanov <mikee2988@yahoo.com>
2021-04-19 14:42:29 +0300

    added review validation

---
+++ src/middlewares/validation/reviewValidation.js
@@ -0,0 +1,25 @@
+import { check, validationResult } from "express-validator";
+import ErrorResponse from "../../utils/errorResponse.js";
+
+export const validateReview = [
+  check("comment").trim().notEmpty().withMessage("comment cannot be empty"),
+  check("rate")
+    .trim()
+    .notEmpty()
+    .isFloat({ min: 0, max: 5 })
+    .withMessage("rating must be a number between 0 and 5"),
+  check("productId").trim().notEmpty().withMessage("productId cannot be empty"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new ErrorResponse(
+        `Something went wrong with validation`,
+        400,
+        "reviewValidation"
+      );
+      error.errList = errors;
+      return next(error);
+    }
+    next();
+  },
+];
