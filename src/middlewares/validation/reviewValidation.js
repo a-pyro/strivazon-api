2107ec53c209bec7874c969a352c2dd512abad65
Mihai Ivanov <mikee2988@yahoo.com>
2021-04-19 16:22:31 +0300

    first test with schemaValidaiton

--- src/middlewares/validation/reviewValidation.js
+++ src/middlewares/validation/reviewValidation.js
@@ -1,4 +1,4 @@
-import { check, validationResult } from "express-validator";
+import { check, validationResult, checkSchema } from "express-validator";
 import ErrorResponse from "../../utils/errorResponse.js";
 
 export const validateReview = [
@@ -23,3 +23,21 @@ export const validateReview = [
     next();
   },
 ];
+
+const validateSchemaReview = {
+  comment: {
+    notEmpty: true,
+    errorMessage: "comment cannot be empty",
+  },
+  rate: {
+    notEmpty: true,
+    isFloat: { min: 1, max: 5 },
+    errorMessage: "rating must be a number between 1 and 5",
+  },
+  productId: {
+    notEmpty: true,
+    errorMessage: "productId cannot be empty",
+  },
+};
+
+export const schemaReview = checkSchema(validateSchemaReview);
